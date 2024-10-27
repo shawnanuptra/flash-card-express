25ada04c229ae67bcd8e8ccab3930401ffa30884
Shawn <shawn.martin@sunderlandsoftwarecity.com>
2024-10-27 22:57:37 +0000

    renamed variables

--- script.js
+++ script.js
@@ -1,11 +1,10 @@
 // async top-level because fetch is async
 (async () => {
 
-	// const flashCards = document.getElementById("FlashCards");
 	const res = await fetch('http://localhost:3000/')
-	let cards = await res.json();
+	let flashcards = await res.json();
 
-	const flashCards = cards.map(card => `<a href='/flashcard/?slug=` + card.slug + `' class='flashcard'>` + card.name + `</a>`)
+	const flashcardLinks = flashcards.map(card => `<a href='/flashcard/?slug=` + card.slug + `' class='flashcard'>` + card.name + `</a>`)
 
-	document.getElementById("FlashCards").innerHTML = flashCards;
+	document.getElementById("flashcards").innerHTML = flashcardLinks;
 })()
